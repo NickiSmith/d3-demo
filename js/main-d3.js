ce55e35f4f4d1a8889f2788959d52c3ad2bf1d4f
Nicki Smith <jnsmith22@wisc.edu>
2019-09-30 19:35:02 -0400

    Added new cities layer and layer control

---
+++ js/main-d3.js
@@ -0,0 +1,10 @@
+/* d3 Demo GEOG 575 Nicki Smith */
+
+ //execute script when window is loaded
+window.onload = function(){
+
+    var container = d3.select("body") //get the <body> element from the DOM
+
+};
+
+
